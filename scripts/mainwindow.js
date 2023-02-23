6bad996f88b757bca9bc309ecb7b89a37f451702
PredragWebDev <pedjolinodev@gmail.com>
2023-02-23 15:59:44 -0800

    modify

--- scripts/mainwindow.js
+++ scripts/mainwindow.js
@@ -1,94 +1,105 @@
-    document.addEventListener("DOMContentLoaded", function() {
-        var update = document.getElementById("updatebtn");
+document.addEventListener("DOMContentLoaded", function() {
+    var update = document.getElementById("updatebtn");
 
-        try{
-            update.addEventListener("click", function() {
-                update_func();
-            });
-        } catch(e) {
-            console.log(e);
-        }
-    
-    });
-
-    document.addEventListener("DOMContentLoaded", function() {
-        var questionArea = document.getElementById("question");
-
-        try{
-            questionArea.addEventListener("input", function() {
-                onchangeQuestionArea_func();
-            });
-        } catch(e) {
-            console.log(e);
-        }
-    
-    });
-  
-    function update_func() {
-        var obj = document.getElementById("question");
-        questions = obj.value.split("\n\n");
-
-        var localdatalength = 0;
-  
-        while(localStorage.getItem(localdatalength.toString(10)) != null){
-            
-            localdatalength++;
-        }
-
-        questions.forEach(question => {
-         
-            if(question != null){
-                if(localStorage.getItem(localdatalength.toString(10)) == null){
-                    localStorage.setItem(localdatalength.toString(10), question);
-                    localdatalength++;
-                }
-            }
-            
-
+    try{
+        update.addEventListener("click", function() {
+            update_func();
         });
-
-        alert("The end questions updating!");
-    
+    } catch(e) {
+        console.log(e);
     }
 
-    window.onload = function() {
+});
+
+document.addEventListener("DOMContentLoaded", function() {
+    var questionArea = document.getElementById("question");
+
+    try{
+        questionArea.addEventListener("input", function() {
+            onchangeQuestionArea_func();
+        });
+    } catch(e) {
+        console.log(e);
+    }
+
+});
+
+function update_func() {
+    var obj = document.getElementById("question");
+    questions = obj.value.split("\n\n");
+
+    if(questions.length == 1 && questions[0] == "") return;
+
+    var localdatacount = 0;
+
+    while(localStorage.getItem(localdatacount.toString(10)) != null){
         
-        document.getElementById("question").value = "";
+        localdatacount++;
+    }
 
-        var str_questionlist = localStorage.getItem("questionlist").split(",");
-        var str_answer = "";
-        var str_answerAndquestion = "";
-        var str_question = "";
-         var n_count = 0;
+    questions.forEach(question => {
+        
+        if(question != null || question != ""){
+            if(localStorage.getItem(localdatacount.toString(10)) == null){
+                localStorage.setItem(localdatacount.toString(10), question);
+                localdatacount++;
+            }
+        }
 
-        // alert(localStorage.getItem("who are you?"));
+    });
 
+    alert(localdatacount.toString(10) + " questions hvae just updated successfuly!");
+
+}
+
+window.onload = function() {
+    
+    // load questions user input
+
+    document.getElementById("question").value = "";
+
+    var n_count = 0;
+
+    while(localStorage.getItem(n_count.toString(10)) != null && localStorage.getItem(n_count.toString(10)) != "") {
+        document.getElementById("question").value += localStorage.getItem(n_count.toString(10)) + "\n\n";
+        n_count ++;
+    }
+
+    // load answers and questions
+    var str_questionlist = localStorage.getItem("questionlist");
+    if(str_questionlist != null && str_questionlist != "")  str_questionlist = str_questionlist.split(",");
+    
+    var str_answer = "";
+    var str_answerAndquestion = "";
+
+    if (str_questionlist != null && str_questionlist) {
         str_questionlist.forEach(question => {
-
-            
 
             if(question != "") {
                 str_answer = localStorage.getItem(question);
-
+    
                 str_answerAndquestion += question + "\n\n" + str_answer + "\n\n------------------------------------\n\n";
-
+    
                 document.getElementById("answer").value = str_answerAndquestion;
             }
             
         });
         
-    };
-
-    function onchangeQuestionArea_func() {
-        
-        var questions = document.getElementById("question").value.split("\n\n");
-
-        if(questions.length > 1) {
-            if(questions[questions.length-1] != "")
-                document.getElementById("numofquestion").innerText = questions.length.toString(10) + " Questions";
-                
-        } else {
-            
-            document.getElementById("numofquestion").innerText = questions.length.toString(10) + " Question";
-        }
     }
+
+};
+
+function onchangeQuestionArea_func() {
+    
+    var questions = document.getElementById("question").value.split("\n\n");
+
+    if(questions.length > 1) {
+        if(questions[questions.length-1] != "")
+            document.getElementById("numofquestion").innerText = questions.length.toString(10) + " Questions";
+
+    } else {
+        
+        document.getElementById("numofquestion").innerText = questions.length.toString(10) + " Question";
+    }
+}
+
--- scripts/popup.js
+++ scripts/popup.js
@@ -18,6 +18,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     try{
         startButton.addEventListener("click", function() {
+            flagForStop = false;
             sendQuestion_func();
         })
     } catch(e) {
@@ -49,13 +50,8 @@ function sendQuestion_func() {
     
     var n_count = 0;
     var str_question = "";
-    var str_answer = "";
     var n_totalquestions = 0;
-    var str_questionlist = localStorage.getItem("questionlist");
-
-    if(str_questionlist == null) {
-        str_questionlist = "";
-    }
+    var n_curquestions = 0;
 
     document.getElementById("runORstop").innerText = "  Running...";
 
@@ -65,29 +61,33 @@ function sendQuestion_func() {
         n_totalquestions++;
     }
 
-    ////////////////-----start-----//////////////////////////////////////
+    // get number of current questions
 
     while(localStorage.getItem(n_count.toString(10)) != null) {
+        n_count ++;
+    }
+
+    n_curquestions = n_count;
+
+    if(n_curquestions == 0) {
+        alert("No questions. Please input the questions.");
+        document.getElementById("runORstop").innerText = "The end";
+        return;
+    }
+
+    n_count = 0;
+
+    ////////////////-----start-----//////////////////////////////////////
+
+    for(n_count = 0 ; n_count < n_curquestions ; n_count ++) {
         
         str_question = localStorage.getItem(n_count.toString(10));
         
         ///////////--------get answer from chatgpt-----------////////////
 
-        str_answer = "answer";
+        get_answer_from_chatgpt_func(str_question, n_count, n_curquestions, n_totalquestions);
 
-        /////////////add the answer to local storage/////////////////////
         
-        str_questionlist += str_question + ",";
-
-        localStorage.setItem("questionlist", str_questionlist);
-
-        localStorage.setItem(str_question, str_answer);
-
-        n_count++;
-
-        document.getElementById("anANDqu").innerText = n_count.toString(10) + "/" + n_totalquestions.toString(10) + " answers saved. ";
-
-
         ///////////////////////stop/////////////////////////////////////
 
         if(flagForStop == true){
@@ -98,11 +98,28 @@ function sendQuestion_func() {
         }
 
     }
-
-    localStorageModify_func(n_count);
-    document.getElementById("runORstop").innerText = "  End";
-
     
+}
+
+function handleAnswer(str_question, str_answer) {
+    var str_questionlist = localStorage.getItem("questionlist");
+
+    if(str_questionlist == null) {
+        str_questionlist = "";
+    }
+
+    /////////////add the answer to local storage/////////////////////
+        
+    str_questionlist += str_question + ",";
+
+    localStorage.setItem("questionlist", str_questionlist);
+
+    localStorage.setItem(str_question, str_answer);
+    
+}
+
+function handleError(str_question, error) {
+    alert(error);
 }
 
 function localStorageModify_func(index) {
@@ -117,4 +134,58 @@ function localStorageModify_func(index) {
     for(; index0 < index; index0 ++) {
         localStorage.removeItem(index0.toString(10));
     }
+}
+
+async function get_answer_from_chatgpt_func(question, n_count, n_curquestions, n_totalquestions) {
+
+    const apiKey = "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";
+
+    const url = "https://api.openai.com/v1/completions";
+    var bodyData = {
+      "model": "text-davinci-003",
+      "max_tokens": 800,
+      "temperature": 0.5,
+      "prompt": question
+    };
+
+    // remove stream from bodyData
+    var str_bodyData = JSON.stringify(bodyData);
+  
+    fetch(url, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + apiKey
+        },
+        body: str_bodyData
+    })
+    .then((response) => response.body)
+    .then((body) => {
+      const reader = body.getReader();
+
+        reader.read().then(({ done, value }) => {
+            // When no more data needs to be consumed, close the stream
+            if (done) {return;}
+            // Enqueue the next data chunk into our target stream
+            var stream = new TextDecoder().decode(value);//.substring(6);
+            var returnAnswer = JSON.parse(stream).choices[0].text;
+
+            returnAnswer = returnAnswer.replace("\n\n", "");
+
+            handleAnswer(question, returnAnswer);
+
+            document.getElementById("anANDqu").innerText = (n_count + 1).toString(10) + "/" + n_totalquestions.toString(10) + " answers saved. ";
+            localStorageModify_func(n_count);
+        
+            if((n_count + 1)== n_curquestions) {
+                document.getElementById("runORstop").innerText = "The  End";
+            }
+            console.log(string, typeof string);
+        });
+      }
+    ).catch(err => {
+      console.log("error" + err);
+    });
+
 }
